f1712606bfe18149b955835222f27c9afc7eb646
YAMITRAO <work.aky@gmail.com>
2024-03-20 09:00:35 +0530

    loading msg added

--- src/App.js
+++ src/App.js
@@ -7,20 +7,29 @@ import './App.css';
 function App() {
   
  const [dummyMovies, setDummyMovies] = useState([]);
-  const ApiData = () => {
-    fetch("https://swapi.dev/api/films/").then( (response) => {
-     return response.json()
-    }).then( (data) => { console.log(data.results)
-       setDummyMovies(data.results)});
-  }
-
+ const[isLoading , setIsLoading] = useState(false);
+  // const ApiData = () => {
+  //   fetch("https://swapi.dev/api/films/").then( (response) => {
+  //    return response.json()
+  //   }).then( (data) => { console.log(data.results)
+  //      setDummyMovies(data.results)});
+  // }
+   async function ApiData ( ) {
+    setIsLoading(true);
+     const response = await fetch("https://swapi.dev/api/films/");
+     const data = await response.json();
+     setDummyMovies(data.results);
+     setIsLoading(false)
+   }
   return (
     <React.Fragment>
       <section>
         <button onClick={ApiData}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!isLoading && dummyMovies.length > 0 && <MoviesList movies={dummyMovies} />}
+        {!isLoading && dummyMovies.length <= 0 && <p>No Data Here</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
